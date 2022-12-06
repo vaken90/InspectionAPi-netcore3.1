6d048610ca411c44f124e837d7c8a0f5a4163e57
vaken90 <65409856+vaken90@users.noreply.github.com>
2022-12-06 17:50:53 +0100

    first commit

---
+++ InspectionAPI/Status.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class Status
+    {
+
+        public int Id { get; set; }
+
+        [StringLength(20)]
+
+        public string StatusOption { get; set; } = string.Empty;
+
+
+    }
+}
